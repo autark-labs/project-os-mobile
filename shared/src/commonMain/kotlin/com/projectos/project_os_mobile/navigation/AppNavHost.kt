0f4d57d824699ce30b2d56d9c91eb88c3cd04768
Joshua Smith <joshua.m.smith292.mil@army.mil>
2026-06-24 15:12:51 -0500

    build resources

--- shared/src/commonMain/kotlin/com/projectos/project_os_mobile/navigation/AppNavHost.kt
+++ shared/src/commonMain/kotlin/com/projectos/project_os_mobile/navigation/AppNavHost.kt
@@ -3,6 +3,7 @@ package com.projectos.project_os_mobile.navigation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -10,7 +11,7 @@ import androidx.navigation.compose.composable
 import com.projectos.project_os_mobile.HomePage
 
 enum class Routes(val icon: ImageVector, val label: String) {
-    HOME(), APPS
+    HOME(icon = ), APPS
 }
 
 val STARTING_PAGE = Routes.APPS
