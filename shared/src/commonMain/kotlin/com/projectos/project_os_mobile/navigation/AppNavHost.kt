fbf9b605ed3aee4b3595ecedcfd3e1fbbfc46cb4
Joshua Smith <joshua.m.smith292.mil@army.mil>
2026-06-24 15:23:01 -0500

    build route enum with icons

--- shared/src/commonMain/kotlin/com/projectos/project_os_mobile/navigation/AppNavHost.kt
+++ shared/src/commonMain/kotlin/com/projectos/project_os_mobile/navigation/AppNavHost.kt
@@ -3,15 +3,23 @@ package com.projectos.project_os_mobile.navigation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.projectos.project_os_mobile.HomePage
+import com.projectos.project_os_mobile.shared.Res
+import com.projectos.project_os_mobile.shared.apps_icon
+import com.projectos.project_os_mobile.shared.home_icon
+import com.projectos.project_os_mobile.shared.settings_icon
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
 
 
-enum class Routes(val icon: ImageVector, val label: String) {
-    HOME(icon = ), APPS
+enum class Routes(val icon: DrawableResource, val label: String) {
+    HOME(Res.drawable.home_icon, "Home"),
+    APPS(Res.drawable.apps_icon, "Apps"),
+    SETTINGS(Res.drawable.settings_icon, "Settings"),
 }
 
 val STARTING_PAGE = Routes.APPS
