a9cc45e6bf2bd52ee658e155d40e096e8193e711
Jackson Robertson <Jackson.H.Robertson.mil@army.mil>
2026-06-24 12:26:20 -0500

    Add basic routing for the mobile app

--- shared/src/commonMain/kotlin/com/projectos/project_os_mobile/App.kt
+++ shared/src/commonMain/kotlin/com/projectos/project_os_mobile/App.kt
@@ -1,49 +1,28 @@
 package com.projectos.project_os_mobile
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
-import project_os_mobile.shared.generated.resources.Res
-import project_os_mobile.shared.generated.resources.compose_multiplatform
+
+enum class Routes {
+    HOME, APPS
+}
 
 @Composable
 @Preview
 fun App() {
+    val navController = rememberNavController()
+
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        NavHost (navController = navController, startDestination = Routes.HOME.name) {
+            composable(Routes.HOME.name)  { HomePage { navController.navigate(it) } }
+            composable(Routes.APPS.name)  { Text("Hello!") }
         }
     }
 }
